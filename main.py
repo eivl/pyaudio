f90a1dc3adb2d078bce023e4738864ff7f9ea562
Mario Simonsen <mariosim93@gmail.com>
2024-11-26 20:12:35 +0100

    Imported standard textual widgets

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-import this
+from textual.app import App, ComposeResult 
+from textual.containers import ScrollableContainer
+from textual.events import Mount
+from textual.reactive import reactive
+from textual.widgets import Header, Footer, Button, Static
